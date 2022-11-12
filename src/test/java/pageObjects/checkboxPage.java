18f1e2a659c9c076ecf405a38a9ab7571c811847
Dima Smaha <fannifty@gmail.com>
2022-11-12 06:40:15 +0200

    space

--- src/test/java/pageObjects/checkboxPage.java
+++ src/test/java/pageObjects/checkboxPage.java
@@ -3,7 +3,6 @@ package pageObjects;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
